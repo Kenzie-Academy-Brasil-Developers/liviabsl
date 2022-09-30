9d32f63f090f27538d8174df94b09f73df531aa8
liviabsl <liviasoliveira03@gmail.com>
2022-09-30 18:40:57 -0400

    SQLanches entrega

--- crud.sql
+++ crud.sql
@@ -5,30 +5,57 @@
 
 -- 1)
 
-
+INSERT INTO clientes(nome, lealdade)
+	VALUES
+	('Georgia', 0);
 
 -- 2)
 
-
+INSERT INTO pedidos(status, cliente_id)
+	VALUES
+	('Recebido', 6);
 
 -- 3)
 
-
+INSERT INTO produtos_pedidos (pedido_id, produto_id)
+	VALUES
+	(6,1),
+	(6,2),
+	(6,6),
+	(6,8),
+	(6,8);
 
 -- Leitura
 
 -- 1)
 
-
+SELECT c.id, c.nome, c.lealdade, pe.id, pe.status, pe.cliente_id, pr.id, pr.tipo, pr.preço, pr.pts_de_lealdade 
+    FROM clientes c
+    JOIN pedidos pe ON c.id = pe.cliente_id
+    JOIN produtos_pedidos pp ON pp.pedido_id  = pe.id
+    JOIN produtos pr ON pr.id = pp.produto_id
+    WHERE c.nome = 'Georgia'; 
 
 -- Atualização
 
 -- 1)
 
-
+UPDATE
+	clientes
+SET
+	lealdade = (SELECT SUM(pr.pts_de_lealdade )
+FROM clientes c
+JOIN pedidos pe ON c.id = pe.cliente_id
+JOIN produtos_pedidos pp ON pp.pedido_id  = pe.id
+JOIN produtos pr ON pr.id = pp.produto_id
+WHERE c.nome = 'Georgia')
+WHERE id= 6;
 
 -- Deleção
 
 -- 1)
 
-
+DELETE FROM
+	clientes
+WHERE
+	id= 4;
--- selects.sql
+++ selects.sql
@@ -3,10 +3,44 @@
 
 -- 1)
 
+SELECT 
+	pe.id, pe.status, pe.cliente_id, pr.id, pr.nome, pr.tipo, pr."preço", pr.pts_de_lealdade  
+    FROM pedidos pe 
+    JOIN produtos_pedidos pp  on pedido_id=pe.id 
+    JOIN produtos pr on produto_id = pr.id;
+
 -- 2)
+
+SELECT pedido_id 
+    FROM produtos_pedidos pp 
+    JOIN produtos p ON pp.produto_id = p.id 
+    WHERE p.nome = 'Fritas';
 
 -- 3)
 
+SELECT c.nome gostam_de_fritas
+    FROM produtos_pedidos pp 
+    JOIN produtos p ON pp.produto_id = p.id  
+    JOIN pedidos pe ON pe.id = pedido_id 
+    JOIN clientes c ON c.id = pe.cliente_id 
+    WHERE p.nome = 'Fritas';
+
+
 -- 4)
 
+SELECT SUM(p."preço")
+    FROM produtos_pedidos pp 
+    JOIN produtos p ON pp.produto_id = p.id  
+    JOIN pedidos pe ON pe.id = pedido_id 
+    JOIN clientes c ON c.id = pe.cliente_id
+    WHERE c.nome='Laura';
+
 -- 5)
+
+SELECT p.nome, COUNT(*)
+    FROM produtos_pedidos pp 
+    JOIN produtos p ON pp.produto_id = p.id  
+    JOIN pedidos pe ON pe.id = pedido_id 
+    JOIN clientes c ON c.id = pe.cliente_id
+    GROUP BY p.nome
+    ORDER BY p.nome ASC;
